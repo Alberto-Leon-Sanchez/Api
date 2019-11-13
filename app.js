0684e59e6c3117249a568d66579f5f55545bc90f
unknown <albertoleonsanchez@gmail.com>
2019-11-13 17:07:06 +0100

    permisos corps

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const bookRoutes = require('./routes/bookRoutes');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Alow-Methods', 'PUT,GET,POST,OPTIONS');
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/contacts', userRoutes);
